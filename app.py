9abccee5f2e6bd59f63d32acfd4498d5940cade6
me <gary.mathe87@gmail.com>
2024-03-27 16:12:54 +0100

    j'ai lam

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
+
 from file_loading import load_spectrum_file
-import plotly.express as ex
-st.write('Hello World')
+import plotly.express as px
+
+st.write("Hello World")
+print('toto')
 
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file:
     spectrum = load_spectrum_file(uploaded_file)
-    fig = ex.scatter(spectrum.data)
+    fig = px.scatter(spectrum.data)
     st.plotly_chart(fig, use_container_width=True)
